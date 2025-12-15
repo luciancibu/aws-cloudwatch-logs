271195600433bf321facbd032aa8e53cbe800095
Lucian Cibu <luciancibu@yahoo.com>
2025-12-15 23:29:07 +0200

    Code refactor

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -14,26 +14,6 @@ resource "aws_iam_role" "ec2_role" {
   })
 }
 
-data "aws_iam_policy_document" "ec2_s3_policy" {
-  statement {
-    effect = "Allow"
-    actions = [
-      "s3:PutObject",
-      "s3:GetObject",
-      "s3:ListBucket" // allow to list (aws s3 ls) files from bucket e.g. aws s3 ls s3://logs-bucket
-    ]
-    resources = [
-      aws_s3_bucket.logs_deploy.arn,   // for "s3:ListBucket"
-      "${aws_s3_bucket.logs_deploy.arn}/*"
-    ]
-  }
-}
-
-resource "aws_iam_role_policy" "ec2_cloudwatch_s3" {
-  role   = aws_iam_role.ec2_role.id
-  policy = data.aws_iam_policy_document.ec2_s3_policy.json
-}
-
 # CloudWatch Logs full access
 resource "aws_iam_role_policy_attachment" "ec2_cloudwatch_logs_full" {
   role       = aws_iam_role.ec2_role.id
