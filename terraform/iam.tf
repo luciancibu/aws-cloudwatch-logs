433eeae8fd19c0df30fa245ef4df82b14f7e813d
Lucian Cibu <luciancibu@yahoo.com>
2026-01-02 12:14:52 +0200

    Migrate from awslogs to CloudWatch Unified Agent

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -20,6 +20,18 @@ resource "aws_iam_role_policy_attachment" "ec2_cloudwatch_logs_full" {
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
 }
 
+# Allow EC2 (CloudWatch agent) to send metrics to CloudWatch
+resource "aws_iam_role_policy_attachment" "ec2_cloudwatch_agent" {
+  role       = aws_iam_role.ec2_role.id
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
+# For debug -> aws cloudwatch list-metrics --namespace CWAgent
+resource "aws_iam_role_policy_attachment" "ec2_cloudwatch_readonly" {
+  role       = aws_iam_role.ec2_role.id
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchReadOnlyAccess"
+}
+
 resource "aws_iam_instance_profile" "ec2_profile" {
   name = "${var.projectName}-ec2-profile"
   role = aws_iam_role.ec2_role.name
